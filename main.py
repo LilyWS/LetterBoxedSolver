bd496fb404a5c9ba77bead815aeb6584ca21d0e6
LilyWS <stranger1212ss23@gmail.com>
2024-11-03 19:43:02 -0500

    adding sides and word checking through recursion

---
+++ main.py
@@ -0,0 +1,29 @@
+import nltk
+from nltk.corpus import words
+
+#nltk.download("words")
+
+#sideInput = input("Please enter sides, 3 letters followed by | for each side")
+
+sides = [
+    ["e", "o", "s"],
+    ["a", "r", "u"],
+    ["i", "h", "l"],
+    ["z", "m", "b"]
+]
+
+word_list = set(word.lower() for word in words.words())
+possibleWords = set()
+
+def get_side(letter):
+    for side in sides:
+        if letter in side:
+            print(side)
+
+for side in sides:
+    for letter in side:
+        for word in word_list:
+            if word[0] == letter:
+                print(type(word))
+                print(word)
+                get_side(word[0])
